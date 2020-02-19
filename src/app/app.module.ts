fc2fa0f6f1566c531faa468f6c9c45aaa4f4b1ec
Yucker <yucker14n@gmail.com>
2020-02-19 11:51:01 +0100

    Primer Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabeceraComponent,
+    FooterComponent,
+    NavComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCpCyzmeeKEqEAQXiWwx80C_2Fq6oqH_hQ",
+    authDomain: "appproductora.firebaseapp.com",
+    databaseURL: "https://appproductora.firebaseio.com",
+    projectId: "appproductora",
+    storageBucket: "appproductora.appspot.com",
+    messagingSenderId: "78151167860",
+    appId: "1:78151167860:web:f89111207407c595f10621",
+    measurementId: "G-YLD8DTTEE4"
+  },
+  
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
