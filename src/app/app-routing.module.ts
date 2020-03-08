cd524b6c0f8cd5fbcf9795b4aadc461bdd2512d4
Yucker <yucker14n@gmail.com>
2020-03-08 13:58:34 +0100

    Revisar detalles id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,46 +1,47 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { DetallesComponent } from './components/detalles/detalles.component';
-import { ListadoComponent } from './components/listado/listado.component';
-import { HistoriaComponent } from './components/historia/historia.component';
-import { FormpeliculaComponent } from './components/formpelicula/formpelicula.component';
-import { RecordsComponent } from './components/records/records.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component";
+import { DetallesComponent } from "./components/detalles/detalles.component";
+import { ListadoComponent } from "./components/listado/listado.component";
+import { HistoriaComponent } from "./components/historia/historia.component";
+import { FormpeliculaComponent } from "./components/formpelicula/formpelicula.component";
+import { RecordsComponent } from "./components/records/records.component";
+import { EditComponent } from "./components/edit/edit.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: HistoriaComponent
   },
   {
-    path: 'login',
+    path: "login",
     component: LoginComponent
   },
   {
-    path: 'detalles/:id',
+    path: "detalles/:id",
     component: DetallesComponent
   },
   {
-    path: 'catalogo',
+    path: "catalogo",
     component: ListadoComponent
   },
   {
-    path: 'historia',
+    path: "historia",
     component: HistoriaComponent
   },
   {
-    path: 'formpelicula',
+    path: "formpelicula",
     component: FormpeliculaComponent
   },
   {
-    path: 'records',
+    path: "records",
     component: RecordsComponent
-  }
+  },
+  { path: "edit:id", component: EditComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/edit/edit.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from "@angular/core";
+import { Pelicula } from "src/app/interfaces/pelicula";
+import { PeliculasService } from "src/app/services/peliculas.service";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+  selector: "app-edit",
+  templateUrl: "./edit.component.html",
+  styleUrls: ["./edit.component.css"]
+})
+export class EditComponent implements OnInit {
+  pelicula: Pelicula;
+  reparto: string[];
+  director: string;
+  titulo: string;
+  anyo: number;
+  espectadores: number;
+
+  constructor(
+    private peliculasService: PeliculasService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.getPelicula();
+  }
+  getPelicula() {
+    this.director = this.titulo = "";
+    this.anyo = this.espectadores = 0;
+    this.reparto = [];
+    const id = this.route.snapshot.paramMap.get("id");
+    this.peliculasService.getPelicula(id).subscribe(peliSnapshot => {
+      this.pelicula = peliSnapshot.payload.data() as Pelicula;
+      this.getReparto();
+    });
+
+    this.titulo = this.pelicula.titulo;
+    this.director = this.pelicula.director;
+    this.espectadores = this.pelicula.espectadores;
+    this.reparto = this.pelicula.reparto;
+    this.anyo = this.pelicula.year;
+  }
+
+  getReparto() {
+    this.reparto = [];
+    this.pelicula.reparto.forEach(element => {
+      this.reparto.push(element);
+    });
+  }
+}
